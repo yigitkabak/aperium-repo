89761c1b170751958109bc56b12e18eac6a3882a
Yiğit KABAK <yigitkabak@tuta.io>
2025-08-06 12:25:55 +0300

    Update index.js

--- modules/express-test/index.js
+++ modules/express-test/index.js
@@ -1,48 +1,36 @@
 // aperium_modules/express-test/index.js
 
-const path = require('path');
-const url = require('url');
+const http = require('http');
 
-// This function simulates the behavior of the real Express module.
-// It returns a mock application object with `get` and `listen` methods.
-const createMockApp = () => {
+// A simple routing system to mimic Express's app.get()
+const createExpressLikeApp = () => {
     const routes = {};
 
     const app = {
-        // Simulates the `app.get()` method for defining a GET route.
         get: (routePath, handler) => {
             routes[routePath] = handler;
         },
 
-        // Simulates the `app.listen()` method to start the server.
         listen: (port, callback) => {
-            console.log(`Express-test server started on port: ${port}`);
-            
-            // In a real server, this would not block.
-            // We immediately call the callback to match the non-blocking behavior.
-            if (callback) {
-                callback();
-            }
-
-            // A mock request handler to demonstrate the route.
-            console.log("--- Simulating a request to the '/' route ---");
-            const mockReq = { method: 'GET', url: '/' };
-            const mockRes = {
-                send: (message) => {
-                    console.log(`Response from server: "${message}"`);
+            const server = http.createServer((req, res) => {
+                const handler = routes[req.url];
+                if (handler) {
+                    res.send = (message) => {
+                        res.writeHead(200, { 'Content-Type': 'text/plain' });
+                        res.end(message);
+                    };
+                    handler(req, res);
+                } else {
+                    res.writeHead(404, { 'Content-Type': 'text/plain' });
+                    res.end('404 Not Found');
                 }
-            };
+            });
 
-            const handler = routes['/'];
-            if (handler) {
-                handler(mockReq, mockRes);
-            }
-            console.log("----------------------------------------------");
+            server.listen(port, callback);
         }
     };
 
     return app;
 };
 
-// Export the factory function, as is standard with Express.
-module.exports = createMockApp;
+module.exports = createExpressLikeApp;
